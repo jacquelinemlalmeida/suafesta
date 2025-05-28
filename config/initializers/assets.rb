e38688122bd7f68e8840e1640e60db4ce7f22331
Jacqueline Almeida <jacquelinealmeida@engefoto.com.br>
2025-05-27 22:31:09 -0300

    fix

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,7 @@
 Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
 Rails.application.config.assets.precompile += %w[application.js application.css]
+Rails.application.config.assets.configure do |env|
+  if env.respond_to?(:unregister_processor)
+    env.unregister_processor 'text/css', Sprockets::SassProcessor rescue nil
+  end
+end
