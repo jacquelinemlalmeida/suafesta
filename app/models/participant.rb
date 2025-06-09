5ce2c817a58839b0964423d0ec0df54b7585002a
Jacqueline Almeida <jacquelinealmeida@engefoto.com.br>
2025-06-09 09:36:06 -0300

    RG é único

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,5 +2,6 @@ class Participant < ApplicationRecord
   has_many :guests, inverse_of: :participant, dependent: :destroy
   accepts_nested_attributes_for :guests, allow_destroy: true
 
-   validates :full_name, :rg, presence: true
+  validates :full_name, :rg, presence: true
+  validates :rg, uniqueness: true
 end
