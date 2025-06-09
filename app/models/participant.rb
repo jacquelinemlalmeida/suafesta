1e73ce3cc291a49f4e3dc7d0c562caf73e8a6f99
Jacqueline Almeida <jacquelinealmeida@engefoto.com.br>
2025-06-09 09:39:37 -0300

    RG é único

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,4 +4,19 @@ class Participant < ApplicationRecord
   accepts_nested_attributes_for :guests, allow_destroy: true
 
   validates :full_name, :rg, presence: true
+
+  before_save :uniqueness_rg
+
+  private
+
+  def uniqueness_rg
+    sanitized_rg = rg.to_s.gsub(/[^a-zA-Z0-9]/, '').downcase
+
+    self.rg = sanitized_rg
+
+    if Participant.where.not(id: id).exists?(rg: sanitized_rg)
+      errors.add(:rg, "já está em uso")
+      throw :abort
+    end
+  end
 end
